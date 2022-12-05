ca3095d768d7d5025536687e9cc131b2d304f07e
omerokumus <omerokumus3@gmail.com>
2022-12-05 09:10:37 +0300

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Ömer Faruk Okumuş on 5.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
