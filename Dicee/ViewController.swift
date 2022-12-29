95ba5a2c41be8908050f6802d3d6efcedb7ca374
omerokumus <omerokumus3@gmail.com>
2022-12-29 18:02:14 +0300

    Dice Roller

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -1,19 +1,35 @@
 //
 //  ViewController.swift
-//  Dicee
+//  Dicee-iOS13
 //
-//  Created by Ömer Faruk Okumuş on 5.12.2022.
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var dice1Image: UIImageView!
+    @IBOutlet weak var dice2Image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func onClickRollButton(_ sender: UIButton) {
+        let dices = [getImage("DiceOne"), getImage("DiceTwo"), getImage("DiceThree")
+                     , getImage("DiceFour"), getImage("DiceFive"), getImage("DiceSix")]
+        
+        dice1Image.image = dices[Int.random(in: 0..<dices.count)]
+        dice2Image.image = dices[Int.random(in: 0..<dices.count)]
+    }
+    
+    func getImage(_ imageName: String) -> UIImage {
+        return UIImage(imageLiteralResourceName: imageName)
+    }
+    
 }
 
